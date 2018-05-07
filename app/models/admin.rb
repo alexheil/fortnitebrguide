9e5e1c4db6e28c6e5f6183e2f2be8e7c092c9591
alexheil <aheil4250@gmail.com>
2018-05-06 20:45:09 -0400

    second

---
+++ app/models/admin.rb
@@ -0,0 +1,6 @@
+class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :recoverable, :rememberable, :trackable, :validatable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+end
